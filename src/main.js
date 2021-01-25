5ad683e07d9fd7a022e26f6f4b48591fdcc55be0
zzy <zhouzhenyu@shuinfo.com>
2021-01-25 19:52:30 +0800

    webpack

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style/style01.css';
+
+const _ = require('lodash');
+var array = [1];
+var other = _.concat(array, 2, [3], [[4]]);
+ 
+console.log(other);
+// => [1, 2, 3, [4]]
+ 
+console.log(array);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'learn-webpack'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
